790ece8c9176f269797f1bba827f95968d56f506
Bauti <bautistamgutierrez@gmail.com>
2024-09-16 10:08:48 -0300

    Subiendo la version inicial del frontend

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto
+Este proyecto se corresponde a un sistema de gestión odontológica que inluye gestión de pacientes, turnos y análisis estadístico.
---
+++ app/frontend/javascript/turnero.js
@@ -0,0 +1,20 @@
+const addTurnoButton = document.getElementById('add-turno-button-id')
+const addTurnoModal = document.querySelector('.modal-crear-turno')
+const closeModalButton = document.querySelector('.close-modal')
+
+console.log(addTurnoButton)
+console.log(addTurnoModal)
+console.log(closeModalButton)
+
+
+// https://stackoverflow.com/questions/242608/disable-browsers-vertical-and-horizontal-scrollbars
+
+addTurnoButton.addEventListener('click', () => {
+    addTurnoModal.setAttribute('open', '')
+    document.documentElement.style.overflow = 'hidden'
+})
+
+closeModalButton.addEventListener('click', () => {
+    addTurnoModal.removeAttribute('open')
+    document.documentElement.style.overflow = 'visible'
+})
