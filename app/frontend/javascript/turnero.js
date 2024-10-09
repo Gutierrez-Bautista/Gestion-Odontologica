2660aba21036a31afc7b4f73b0fc5efe993c907c
Bauti <bautistamgutierrez@gmail.com>
2024-10-09 20:44:17 -0300

    dando formato a archivos, eliminando archivos innecesarios y actualizando .gitignore

--- app/frontend/javascript/turnero.js
+++ app/frontend/javascript/turnero.js
@@ -1,20 +1,19 @@
-const addTurnoButton = document.getElementById('add-turno-button-id')
-const addTurnoModal = document.querySelector('.modal-crear-turno')
-const closeModalButton = document.querySelector('.close-modal')
+const addTurnoButton = document.getElementById("add-turno-button-id");
+const addTurnoModal = document.querySelector(".modal-crear-turno");
+const closeModalButton = document.querySelector(".close-modal");
 
-console.log(addTurnoButton)
-console.log(addTurnoModal)
-console.log(closeModalButton)
-
+console.log(addTurnoButton);
+console.log(addTurnoModal);
+console.log(closeModalButton);
 
 // https://stackoverflow.com/questions/242608/disable-browsers-vertical-and-horizontal-scrollbars
 
-addTurnoButton.addEventListener('click', () => {
-    addTurnoModal.setAttribute('open', '')
-    document.documentElement.style.overflow = 'hidden'
-})
+addTurnoButton.addEventListener("click", () => {
+  addTurnoModal.setAttribute("open", "");
+  document.documentElement.style.overflow = "hidden";
+});
 
-closeModalButton.addEventListener('click', () => {
-    addTurnoModal.removeAttribute('open')
-    document.documentElement.style.overflow = 'visible'
-})
+closeModalButton.addEventListener("click", () => {
+  addTurnoModal.removeAttribute("open");
+  document.documentElement.style.overflow = "visible";
+});
