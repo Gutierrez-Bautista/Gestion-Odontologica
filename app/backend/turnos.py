2ab34744ec28a230cd2bdb4f604b1fa00016238f
canapro <santicanavesio1@gmail.com>
2024-10-14 13:35:10 -0300

    subiendo actualizacion de alta de turnos

--- app/backend/turnos.py
+++ app/backend/turnos.py
@@ -3,7 +3,7 @@ import tablas
 
 DB_NAME = 'clinica.db'
 
-def solicitar_info(fecha_turno):
+def solicitar_info(fecha_turno1, fecha_turno2):
     """
     Solicita información de un turno específico según la fecha proporcionada.
     """
@@ -12,40 +12,44 @@ def solicitar_info(fecha_turno):
         connection = sqlite3.connect(DB_NAME)
         cursor = connection.cursor()
 
-        query = "SELECT * FROM turnos WHERE fecha = ?"
-        cursor.execute(query, (fecha_turno,))
+        query = "SELECT * FROM turnos WHERE fecha between fecha_turno1 and fecha_turno2"
+        cursor.execute(query, (fecha_turno1,fecha_turno2))
         resultados = cursor.fetchall()
         cursor.close()
         connection.close()
         if resultados:
             return resultados
         else:
-            return f"No se encontraron turnos para la fecha {fecha_turno}."
-
+            #return f"No se encontraron turnos para la fecha {fecha_turno}."
+            pass
     except sqlite3.Error as e:
         return f"Error al solicitar la información: {e}"
 
 
-def agregar_info(nombre, fecha, hora):
+def agregar_info(nombre, fecha, hora, motivo):
     """
     Agrega un nuevo turno a la base de datos.
     """
     try:
         connection = sqlite3.connect(DB_NAME)
         cursor = connection.cursor()
-        query = "INSERT INTO turnos (nombre, fecha, hora) VALUES (?, ?, ?)"
-        cursor.execute(query, (nombre, fecha, hora))
-        connection.commit()
-        cursor.close()
-        connection.close()
-
-        return "Turno agregado exitosamente."
-    
+        cursor.execute("select * from turnos where fecha = ? and hora = ?", (fecha, hora))
+        valid = cursor.fetchall()
+        if len(valid) != 0:
+            query = "INSERT INTO turnos (nombre, fecha, hora, motivo) VALUES (?, ?, ?,?)"
+            cursor.execute(query, (nombre, fecha, hora))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return "Turno agregado exitosamente."
+        else:
+            return "El horario de ese turno ya esta ocupado"
+        
     except sqlite3.Error as e:
         return f"Error al agregar el turno: {e}"
 
 
-def eliminar_turno_por_fecha(fecha_turno):
+def eliminar_turno_por_fecha(fecha_turno, t_horario):
     """
     Elimina un turno según la fecha proporcionada.
     """
@@ -54,8 +58,8 @@ def eliminar_turno_por_fecha(fecha_turno):
         connection = sqlite3.connect(DB_NAME)
         cursor = connection.cursor()
 
-        query = "DELETE FROM turnos WHERE fecha = ?"
-        cursor.execute(query, (fecha_turno,))
+        query = "DELETE FROM turnos WHERE fecha = ? and horario = ?"
+        cursor.execute(query, (fecha_turno,t_horario))
         connection.commit()
 
         if cursor.rowcount > 0:
