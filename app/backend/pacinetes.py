c2b30f31bc7b5e0efa0b79fefd01fa1ab4496338
Bauti <bautistamgutierrez@gmail.com>
2024-10-19 23:50:45 -0300

    comenzando la implementacion del alta de pacientes, modificando las tablas de la bbdd y dejando comentario de lo que se necesita agregar en el backend

--- app/backend/pacinetes.py
+++ app/backend/pacinetes.py
@@ -19,7 +19,7 @@ def alta_paciente(nombre_apellido,telefono,email,edad,dni,domicilio,fecha_nacimi
             valid = cursor.fetchall()
             return ['paciente cargado','dataUpload',200]
         else: 
-            cursor.execute('update table Pacientes set nombre_apellido = ? ,telefono = ? ,email = ? ,edad = ? ,dni = ? ,domicilio = ? ,fecha_nacimiento = ? ,posee_pami = ? where nombre_apellido = ?'(nombre_apellido,telefono,email,edad,dni,domicilio,fecha_nacimiento,posee_pami,nombre_apellido))
+            cursor.execute('update table Pacientes set nombre_apellido = ? ,telefono = ? ,email = ? ,edad = ? ,dni = ? ,domicilio = ? ,fecha_nacimiento = ? ,posee_pami = ? where nombre_apellido = ?', (nombre_apellido,telefono,email,edad,dni,domicilio,fecha_nacimiento,posee_pami,nombre_apellido))
             valid = cursor.fetchall()
             return ['paciente actualizado','dataUpload',200]
     except sqlite3.Error as e:
@@ -31,7 +31,7 @@ def alta_paciente(nombre_apellido,telefono,email,edad,dni,domicilio,fecha_nacimi
 
 
 def consul_pacente (id_paciente, nom_paciente, pami_paciente):
-    print(id_paciente, nom_paciente, pami_paciente)
+    # print(id_paciente, nom_paciente, pami_paciente)
     try:
         connection = sqlite3.connect(DB_NAME)
         cursor = connection.cursor()
@@ -46,21 +46,21 @@ def consul_pacente (id_paciente, nom_paciente, pami_paciente):
                 if not valid:
                     return ['Cliente no encontrado', 'noDataFound', 403]
                 else:
-                    return valid
+                    return ['cliente encontrado', valid[0], 200]
             elif nom_paciente is not None:
                 cursor.execute(query + 'where nombre_apellido =?',(nom_paciente,))
                 valid = cursor.fetchall()
                 if not valid:
                     return ['Cliente no encontrado', 'noDataFound', 403]
                 else:
-                    return valid
+                    return ['cliente encontrado', valid, 200]
             elif pami_paciente is not None:
                 cursor.execute(query + 'where posee_pami =?',(pami_paciente,))
                 valid = cursor.fetchall()
                 if not valid:
                     return ['Cliente no encontrado', 'noDataFound', 403]
                 else:
-                    return valid
+                    return ['cliente encontrado', valid, 200]
             else:
                 return ['argumento no pasado', 'argNotPassed', 412]
         except sqlite3.Error as e:
